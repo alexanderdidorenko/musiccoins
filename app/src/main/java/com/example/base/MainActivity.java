82831f6c1075888eeb7e14226c075e119022347e
alexanderdidorenko <ltpddwk@gmail.com>
2024-04-08 20:36:08 +0300

    Update to 0.02

--- app/src/main/java/com/example/base/MainActivity.java
+++ app/src/main/java/com/example/base/MainActivity.java
@@ -1,36 +1,53 @@
 package com.example.base;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.app.Dialog;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.base.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    int currentPosition = 0;
+    Dialog dialog;
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.phonk);
-        binding.button3.setOnClickListener(new View.OnClickListener() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView3);
+        navController = navHostFragment.getNavController();
+        binding.imageView4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                currentPosition = mediaPlayer.getCurrentPosition();
-                mediaPlayer.pause();
+                dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.paskhalko_dialog);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialog_background));
+                }
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.show();
+                navController.navigate(R.id.messangerFragment);
             }
         });
-        binding.button2.setOnClickListener(new View.OnClickListener() {
+        binding.imageView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
-                    mediaPlayer.seekTo(currentPosition);
-                    mediaPlayer.start();
-                }
+                navController.navigate(R.id.musicFragment);
+            }
+        });
+        binding.imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.coming_soon);
             }
         });
     }
