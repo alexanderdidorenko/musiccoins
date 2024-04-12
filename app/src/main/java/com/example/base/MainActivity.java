ae1d7ddd4075459a17c1a1ef5a6a21d785c9f291
alexanderdidorenko <ltpddwk@gmail.com>
2024-04-12 15:51:11 +0300

    Update to 0.025

--- app/src/main/java/com/example/base/MainActivity.java
+++ app/src/main/java/com/example/base/MainActivity.java
@@ -1,54 +1,56 @@
 package com.example.base;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Dialog;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.base.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    Dialog dialog;
-    private NavController navController;
+    private Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView3);
-        navController = navHostFragment.getNavController();
-        binding.imageView4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog = new Dialog(MainActivity.this);
-                dialog.setContentView(R.layout.paskhalko_dialog);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialog_background));
-                }
-                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                dialog.show();
-                navController.navigate(R.id.messangerFragment);
+        dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.whatnew);
+        int dialogWidth = getResources().getDimensionPixelSize(R.dimen.dialog_width);
+        Button closeDialog = dialog.findViewById(R.id.button);
+        closeDialog.setOnClickListener(view -> dialog.dismiss());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialog_background));
+        }
+        dialog.getWindow().setLayout(dialogWidth, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.show();
+        replaceFragment(new MusicFragment());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setSelectedItemId(R.id.main);
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.playlist) {
+                replaceFragment(new MessangerFragment());
             }
-        });
-        binding.imageView5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.musicFragment);
+            else if (itemId == R.id.main) {
+                replaceFragment(new MusicFragment());
             }
-        });
-        binding.imageView6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.coming_soon);
+            else if (itemId == R.id.sets) {
+                replaceFragment(new coming_soon());
             }
+            return true;
         });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.linear_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
